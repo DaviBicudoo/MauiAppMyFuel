33a30f85786df680b7984e1ea5ea477ebaf4f9e2
Usuario <Usuario@CPD02-M20>
2024-02-26 15:54:02 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMyFuel/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiAppMyFuel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double gasoline = Convert.ToDouble(txt_gasoline.Text);
+            double ethanol = Convert.ToDouble(txt_ethanol.Text);
+
+            string message = $"The best option in this case is ";
+
+            if (ethanol > (gasoline * 0.7))
+            {
+                message += "gasoline.";
+            } else
+            {
+                message += "ethanol.";
+            }
+
+            DisplayAlert("Result", message, "Close");
+        }
+    }
+}
